731d1738bbf8e7a93c8062caf5cb24f21b747263
tatulyka <tatulyka@gmail.com>
2022-12-17 19:42:04 +0100

    ison

--- src/components/ListItems/ListItems.jsx
+++ src/components/ListItems/ListItems.jsx
@@ -2,37 +2,17 @@ import React from "react";
 import Card from "../ItemComponent/ItemComponent";
 //import * as classnames from 'classnames';
 import "./ListItems.css";
+import data from "../../data/data.json"
 
 
-const arr = [{
-    price: "28.90",
-    title: "OptikNET Max",
-    speed: "1000/300",
-    description: "odosielania dát až 1000/500"},
-{
-    price: "23.89",
-    title: "OptikNET Extra",
-    speed: "800/160",
-    description: "odosielania dát až 1000/200",
-    isSelected: true},
-{
-    price: "17,89",
-    title: "OptikNET Ideál",
-    speed: "400/80",
-    description: "odosielania dát až 800/160"},
-{
-    price: "12.89",
-    title: "OptikNET Štart",
-    speed: "50/20",
-    description: "odosielania dát až 100/40"}];
+//const arr = ;
 
 class listItems extends React.Component {
     render() { 
   return (
     <div className={this.props.theme.listItems}>
     <div  className="container">
-      {
-       arr.map((card) => 
+      {data.map((card) => 
        <Card key={card.price} price={card.price} title ={card.title} speed ={card.speed} 
        description = {card.description} isSelected = {card.isSelected}></Card> ) 
       }
