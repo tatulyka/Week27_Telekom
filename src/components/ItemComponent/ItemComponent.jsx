4507af56b9dcf4679984479163eb223c9b04c9dd
tatulyka <tatulyka@gmail.com>
2022-12-27 22:09:02 +0100

    activ

--- src/components/ItemComponent/ItemComponent.jsx
+++ src/components/ItemComponent/ItemComponent.jsx
@@ -4,16 +4,12 @@ import "./ItemComponent.сss";
 
 export default class Card extends React.Component {
     render() {
-        const {price, title, speed, description, isSelected, isActiv, setCount, id} = this.props;   
+        const {price, title, speed, description, isSelected} = this.props;   
         let classCard = "";
         if (isSelected) classCard = "selected";  
-        const classActiv = isActiv ?  "activ" : '';
-        function getValueID(){
-            setCount(id)
-        }
     
     return (
-    <div className={`card_body ${classCard} ${classActiv}`}>
+    <div className={`card_body ${classCard}`}>
         <div className="card-text">
             <h1 className="card-price">{price}€</h1>
             <div className="card-period">mesačne</div>
@@ -25,9 +21,8 @@ export default class Card extends React.Component {
             </div>
         </div>
         <div className="card-button">
-            <button className="button" onClick = {getValueID}>Overiť dostupnosť</button>
+            <button className="button">Overiť dostupnosť</button>
         </div>
-        
     </div>  
     );
 }
--- src/components/ListItems/ListItems.jsx
+++ src/components/ListItems/ListItems.jsx
@@ -1,44 +1,26 @@
 import React from "react";
-import { useState} from "react";
 import Card from "../ItemComponent/ItemComponent";
 //import * as classnames from 'classnames';
 import "./ListItems.css";
 import data from "../../data/data.json"
 
 
+//const arr = ;
 
-
-function ListItems(){
-  
-    const [count, setCount] = useState(0);
-
-    const getIndex = (id) => {
-      if (id === count) {
-        return setCount(null);
+class listItems extends React.Component {
+    render() { 
+  return (
+    <div className={this.props.theme.listItems}>
+    <div  className="container">
+      {data.map((card) => 
+       <Card key={card.price} price={card.price} title ={card.title} speed ={card.speed} 
+       description = {card.description} isSelected = {card.isSelected}></Card> ) 
       }
-        return setCount(id);
-
-    }
-    return (
-        <div className="container">
-          {data.map((card) =>
-            <Card key={card.id} 
-              price={card.price} 
-              title={card.title} 
-              speed={card.speed}
-              description={card.description} 
-              isSelected={card.isSelected} 
-              isActiv = {card.id === count}
-              setCount = {setCount} 
-              id = {card.id}> 
-              onClick = {getIndex}
-              </Card>)
-          }
-
-        </div>
       
+    </div>
+    </div>
     );
-  }
-
-export default ListItems;
+}
+}
+export default listItems;    
 
